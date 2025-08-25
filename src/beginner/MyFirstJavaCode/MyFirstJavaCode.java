d0abeb802964154529f037763751abd3c6f14a6d
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-26 03:04:47 +0900

    First commit

---
+++ src/beginner/MyFirstJavaCode/MyFirstJavaCode.java
@@ -0,0 +1,7 @@
+package beginner.MyFirstJavaCode;
+
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+        System.out.println("May the force be with you~");
+    }
+}

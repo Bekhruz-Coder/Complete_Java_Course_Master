91f1e05d739883ea6f54114dce6fd26d6d9ae1c1
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-29 22:32:53 +0900

    Arithmetic operators: +, -, *, /, %.

---
+++ src/beginner/Operators/Operators1.java
@@ -0,0 +1,28 @@
+package beginner.Operators;
+
+public class Operators1 {
+    public static void main(String[] args) {
+
+        String carModel = "Dodge Challenger SRT 392";
+        int price = 14999;
+        int moneyInTheBank = 100000;
+        boolean isDamaged = true;
+
+        System.out.println("Price of a " + carModel + ": $" + price);
+
+        int increasedPrice = price + 1000;
+        System.out.println("The increased Price of a " + carModel + ": $" + increasedPrice);
+
+        int decreasedPrice = price - 1000;
+        System.out.println("The decreased Price of a " + carModel + ": $" + decreasedPrice);
+
+        int twoCarsPrice = price * 2;
+        System.out.println("Two " + carModel + ": $" + twoCarsPrice);
+
+        int dodgesYouCanBuy = moneyInTheBank / price;
+        System.out.println("From the money we have in the back we can buy " + dodgesYouCanBuy + " " + carModel);
+
+        int moneyRemaining = moneyInTheBank % price;
+        System.out.println("Money we would remain after buying " + dodgesYouCanBuy + " " + carModel + ": " + moneyRemaining);
+    }
+}
